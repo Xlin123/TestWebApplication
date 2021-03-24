de6c62d20494ea71ec7a8a377084126956863558
Xavier Lin <xavier.li.1332@gmail.com>
2021-03-23 20:51:07 -0400

    Add project files.

---
+++ WebApplication1/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    //api/controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockWebRepo mockWebRepo = new MockWebRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetCommands()
+        {
+            var commandItems = mockWebRepo.GetCommands();
+            return Ok(commandItems);
+        }
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = mockWebRepo.GetCommandByID(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ WebApplication1/Data/MockWebRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class MockWebRepo : IWebRepo
+    {
+        public IEnumerable<Command> GetCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "ads", Line = "ads", Platform = "sada" },
+                new Command { Id = 1, HowTo = "gsfdgs", Line = "sfg", Platform = "fsgdf" },
+                new Command { Id = 2, HowTo = "ghjk", Line = "jgkj", Platform = "gjk" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandByID(int id)
+        {
+            return new Command { Id = 0, HowTo = "ads", Line = "ads", Platform = "sada" };
+        }
+    }
+}
